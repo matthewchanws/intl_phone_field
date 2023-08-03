a4510ada7b7e35ce1db905a507bf42f2bd7f755b
MatthewChanWS <matthewchan.itparadise@gmail.com>
2023-08-03 12:45:34 +0800

    Test return message 2

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -417,7 +417,7 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
         widget.onChanged?.call(phoneNumber);
       },
       validator: (value) {
-        return 'oomgoomg';
+        return value!.length.toString();
         if (value == null || !isNumeric(value)) return validatorMessage;
         if (!widget.disableLengthCheck) {
           return value.length >= _selectedCountry.minLength && value.length <= _selectedCountry.maxLength
